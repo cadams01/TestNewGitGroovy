44ab99aeb329e8d044b102173cbc4ff8d48f45b1
Chrystal Adams <chrystal.w.adams@gmail.com>
2019-01-13 14:31:14 -0500

    first commit

---
+++ newgitrepo.groovy
@@ -0,0 +1,10 @@
+//New Git Repo for Week6
+def my_colors = ["Purple", "blue", "yellow", "Green", "Orange", "White", "Black", "Teal", "Aqua", "red"]; 
+for (color in my_colors) {
+   if(color == "blue" || color == "red" || color == "yellow"){
+       println("${color} is a primary color.")
+   }else{
+       println("${color} is not a primary color.")
+   }
+}
+
